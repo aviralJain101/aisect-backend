fbb1adbc10fe6c7b1083ae4f2e6ef747e7c36a0e
aviralJain101 <aviraljain.ece18@itbhu.ac.in>
2020-10-09 14:50:05 +0530

    Updated

--- database.js
+++ database.js
@@ -5,9 +5,14 @@ const Sequelize = require('sequelize');
 //     host: 'localhost'
 // })
 
-const sequelize = new Sequelize('lwT9DCPZZ6','lwT9DCPZZ6', 'B9Bvphg0bJ', {
+const sequelize = new Sequelize('AJiheW4U8r','AJiheW4U8r', 'kjazLY17Sg', {
     dialect: 'mysql',
     host: 'remotemysql.com'
 })
+
+// const sequelize = new Sequelize('sql12369704','sql12369704', 'mpUU1EWvhX', {
+//     dialect: 'mysql',
+//     host: 'sql12.freemysqlhosting.net'
+// })
 
 module.exports = sequelize;
--- models/student.js
+++ models/student.js
@@ -11,83 +11,83 @@ const Student = sequelize.define('student', {
     },
     name: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     fatherName: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     address: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     mobileNo: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     totalFees: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     firstInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     secondInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     thirdInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     fourthInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     fifthInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     sixthInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     seventhInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     eighthInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     ninthInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     tenthInstall: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     remFees: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     courseName: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     admissionDate: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     couseDuration: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
     univName: {
         type: Sequelize.STRING,
-        default: '0'
+        defaultValue: '0'
     },
 })
 
